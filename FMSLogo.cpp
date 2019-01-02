f6d68c026cf20be28990e2bcf20f35c559d0d34e
john2015 <yyl_20050115@hotmail.com>
2019-01-03 07:36:58 +0800

    fixes both for windows and macos

--- FMSLogo.cpp
+++ FMSLogo.cpp
@@ -291,18 +291,9 @@ LanguagePair Pairs[] = {
 	{N_LC_RU,N_LOCALIZED_STRINGS_FILE_RU},
 	//{N_LC_PS,N_LOCALIZED_STRINGS_FILE_PS}, //this is Pseudoloc (faked)
 };
-extern unsigned short _binary_localizedstrings_zh_cn_ucs2le_h_start;
-extern unsigned short _binary_localizedstrings_zh_cn_ucs2le_h_end;
-extern char _binary_localizedstrings_zh_cn_ucs2le_h_size;
 
 void CFmsLogo::LoadLocalizedStringFile()
 {
-    unsigned short* pt1 = &_binary_localizedstrings_zh_cn_ucs2le_h_start;
-    
-    unsigned short* pt2 = & _binary_localizedstrings_zh_cn_ucs2le_h_end;
-    unsigned long pt3 = (unsigned long)&_binary_localizedstrings_zh_cn_ucs2le_h_size;
-    unsigned long dpt = pt2-pt1;
-    
 	wxString name;
 
 	//USE SYSTEM LOCALE (for mbtowc)
