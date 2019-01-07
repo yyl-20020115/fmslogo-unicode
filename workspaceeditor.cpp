b18d9e929f746fec0e00dbfd7a7ed649834977f2
yilin <yilin@yilindeMac.local>
2019-01-07 09:11:02 +0800

    auto pending ext is added for linux and macosx

--- workspaceeditor.cpp
+++ workspaceeditor.cpp
@@ -402,6 +402,8 @@ CWorkspaceEditor::Write(
 	//on windows, the sytled text control's line end is \r\n
     //on linux, the styled text controls' line end is \n and the natural saving encoding is native (actually utf-8 is mbcs here)
     //  the natural way of file line end is \n as well, and it will be a one line string if opened by notepad on windows
+    //on macosx, the styled text controls' line end is \n as well
+    //  and the line end is \n as well, so it's ok we treat unix system the same way
 	if (text.length() > 0) {
 		CFileTextStream* cfts = CFileTextStream::OpenForWrite(fileName, this->FTT, true);
 		if (cfts != 0)
