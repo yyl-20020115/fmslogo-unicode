9e9753d4950181a37686a6ad33d2203030c14159
yilin <yyl_20050115@hotmail.com>
2019-01-11 14:56:01 -0800

    linux: changes to application name, title, build packages etc

--- build-deb.sh
+++ build-deb.sh
@@ -5,22 +5,22 @@
 mkdir -p ~/fmslogo-deb
 mkdir -p ~/fmslogo-deb/usr
 mkdir -p ~/fmslogo-deb/usr/bin
-mkdir -p ~/fmslogo-deb/usr/bin/fmslogo
-cp fmslogo ~/fmslogo-deb/usr/bin/fmslogo
-cp startup.logoscript ~/fmslogo-deb/usr/bin/fmslogo
-cp -r logolib ~/fmslogo-deb/usr/bin/fmslogo
-cp -r Examples ~/fmslogo-deb/usr/bin/fmslogo
-cp README.TXT ~/fmslogo-deb/usr/bin/fmslogo
-cp LICENSE.TXT ~/fmslogo-deb/usr/bin/fmslogo
-cp turtle.bmp ~/fmslogo-deb/usr/bin/fmslogo
-cp manual/logohelp.chm ~/fmslogo-deb/usr/bin/fmslogo
+mkdir -p ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp fmslogo-unicode ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp startup.logoscript ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp -r logolib ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp -r Examples ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp README.TXT ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp LICENSE.TXT ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp turtle.bmp ~/fmslogo-deb/usr/bin/fmslogo-unicode
+cp manual/logohelp.chm ~/fmslogo-deb/usr/bin/fmslogo-unicode
 
 mkdir -p ~/fmslogo-deb/usr/share
 mkdir -p ~/fmslogo-deb/usr/share/icons
 cp fmslogo.png ~/fmslogo-deb/usr/share/icons
 mkdir -p ~/fmslogo-deb/usr/share/applications
-cp fmslogo.desktop ~/fmslogo-deb/usr/share/applications
+cp fmslogo-unicode.desktop ~/fmslogo-deb/usr/share/applications
 mkdir -p ~/fmslogo-deb/DEBIAN
 cp control ~/fmslogo-deb/DEBIAN
-dpkg -b ~/fmslogo-deb ./fmslogo_7.3.0.0_i386.deb
+dpkg -b ~/fmslogo-deb ./fmslogo-unicode_7.3.0.0_i386.deb
 
