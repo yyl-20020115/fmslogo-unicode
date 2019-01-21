7792a8911d8b73364a07672ae59fa854b2fa6c24
john2015 <yyl_20050115@hotmail.com>
2019-01-21 11:55:15 +0800

    windows: added WM_USER for linux and macosx

--- logorc.h
+++ logorc.h
@@ -1,3 +1,9 @@
+#ifndef __LOGORC_H__
+#define __LOGORC_H__
+
+#ifndef WM_USER
+#define WM_USER                         0x0400
+#endif
 
 #define WM_MYSCROLLBARCALLBACK         WM_USER + 0
 #define WM_CHECKQUEUE                  WM_USER + 1
@@ -5,3 +11,5 @@
 #define WM_NETWORK_CONNECTSENDACK      WM_USER + 2
 #define WM_NETWORK_CONNECTSENDFINISH   WM_USER + 3
 #define WM_NETWORK_LISTENRECEIVEACK    WM_USER + 4
+
+#endif
