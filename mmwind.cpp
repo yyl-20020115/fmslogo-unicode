7de7d297ce786c4efc366682cc0dfc7e053999e5
john2015 <yyl_20050115@hotmail.com>
2019-01-23 02:01:15 +0800

    windows: bad line format, fixed

--- mmwind.cpp
+++ mmwind.cpp
@@ -23,29 +23,29 @@
 #ifndef USE_PRECOMPILED_HEADER
 #include <wx/string.h>
 #ifdef _WINDOWS
-    #include <windows.h>
+#include <windows.h>
 #endif
-    #include "mmwind.h"
-    #include "argumentutils.h"
-    #include "stringprintednode.h"
-    #include "logocore.h"
-    #include "logodata.h"
-    #include "lists.h"
-    #include "messagebox.h"
-    #include "graphics.h"
-    #include "error.h"
-    #include "init.h"
-    #include "eval.h"
-    #include "parse.h"
-    #include "sound.h"
+#include "mmwind.h"
+#include "argumentutils.h"
+#include "stringprintednode.h"
+#include "logocore.h"
+#include "logodata.h"
+#include "lists.h"
+#include "messagebox.h"
+#include "graphics.h"
+#include "error.h"
+#include "init.h"
+#include "eval.h"
+#include "parse.h"
+#include "sound.h"
 #endif
 
-    #include "screenwindow.h"
-//    #include "debugheap.h"
+#include "screenwindow.h"
+ //    #include "debugheap.h"
 
-    #include "devwind.h"
+#include "devwind.h"
 
-    #include "localizedstrings.h"
+#include "localizedstrings.h"
 
 #ifndef _WINDOWS
 #include <stdio.h>
@@ -59,26 +59,26 @@
 // global variables
 wxString mci_callback;    // MCI callback code
 wxString timer_callback[MAX_TIMERS];      // timer cb malloc'd as needed
-int timer_intervals[MAX_TIMERS] = {0};
-int timer_currents[MAX_TIMERS] = {0};
+int timer_intervals[MAX_TIMERS] = { 0 };
+int timer_currents[MAX_TIMERS] = { 0 };
 
 #ifndef _WINDOWS
 void SIGALRM_Handler(int signo)
 {
-    if(signo == SIGALRM)
-    {
-        for(int i = 0;i<MAX_TIMERS;i++){
-           
-            if(timer_currents[i] == 0){
-                //wxQueueEvent(wxTheApp->GetTopWindow(),new std::wxTimerEvent());
-                //Call Timer's call back
-                timer_currents[i] = timer_intervals[i];
-            }
-            if(timer_currents[i]>0){
-                 timer_currents[i] --;
-            }
-        }
-    }
+	if (signo == SIGALRM)
+	{
+		for (int i = 0; i < MAX_TIMERS; i++) {
+
+			if (timer_currents[i] == 0) {
+				//wxQueueEvent(wxTheApp->GetTopWindow(),new std::wxTimerEvent());
+				//Call Timer's call back
+				timer_currents[i] = timer_intervals[i];
+			}
+			if (timer_currents[i] > 0) {
+				timer_currents[i] --;
+			}
+		}
+	}
 }
 #endif
 
@@ -86,7 +86,7 @@ void SIGALRM_Handler(int signo)
 static HMIDIOUT hMidiOut = 0;
 #endif
 
-bool IsAnyTimerActive() 
+bool IsAnyTimerActive()
 {
 	for (size_t i = 0; i < MAX_TIMERS; i++) {
 		if (timer_callback[i].length() > 0) {
@@ -99,356 +99,354 @@ bool IsAnyTimerActive()
 NODE *lsound(NODE *arg)
 {
 
-    /* open sound and get arg list */
-    NODE *args = car(arg);
+	/* open sound and get arg list */
+	NODE *args = car(arg);
 
-    /* must be a list that contains something */
-    if (is_list(args) && (args != NIL))
-    {
+	/* must be a list that contains something */
+	if (is_list(args) && (args != NIL))
+	{
 
-        /* count items in list and check that they are pairs */
-        int total_args = list_length(args);
-        bool even_args = even_p(total_args);
+		/* count items in list and check that they are pairs */
+		int total_args = list_length(args);
+		bool even_args = even_p(total_args);
 
-        /* if sound creation ok and we have pairs continue */
-        if (even_args)
-        {
-            /* play each freq/duration pair */
-            for (arg = args;
-                 arg != NIL;
-                 arg = cdr(arg))
-            {
-                int duration = 0;
+		/* if sound creation ok and we have pairs continue */
+		if (even_args)
+		{
+			/* play each freq/duration pair */
+			for (arg = args;
+				arg != NIL;
+				arg = cdr(arg))
+			{
+				int duration = 0;
 
-                int hertz = int_arg(arg);
-                if (cdr(arg) != NIL)
-                {
-                    duration = int_arg(arg = cdr(arg));
-                }
-                /* play it */
-                tone(hertz, duration);
-            }
-        }
-        else
-        {
-            ShowMessageAndStop(
-                GetResourceString(L"LOCALIZED_ERROR_SOUND"), 
-                GetResourceString(L"LOCALIZED_ERROR_BADINPUTNOTPAIRED"));
-        }
-    }
-    else
-    {
-        ShowMessageAndStop(
-            GetResourceString(L"LOCALIZED_ERROR_SOUND"),
-            GetResourceString(L"LOCALIZED_ERROR_BADINPUT"));
-    }
+				int hertz = int_arg(arg);
+				if (cdr(arg) != NIL)
+				{
+					duration = int_arg(arg = cdr(arg));
+				}
+				/* play it */
+				tone(hertz, duration);
+			}
+		}
+		else
+		{
+			ShowMessageAndStop(
+				GetResourceString(L"LOCALIZED_ERROR_SOUND"),
+				GetResourceString(L"LOCALIZED_ERROR_BADINPUTNOTPAIRED"));
+		}
+	}
+	else
+	{
+		ShowMessageAndStop(
+			GetResourceString(L"LOCALIZED_ERROR_SOUND"),
+			GetResourceString(L"LOCALIZED_ERROR_BADINPUT"));
+	}
 
 
-    return Unbound;
+	return Unbound;
 }
 
 static
 void
 ThrowGeneralMidiError(
-    unsigned int MidiError
-    )
+	unsigned int MidiError
+)
 {
 	wchar_t midiErrorBuffer[MAX_BUFFER_SIZE + 1] = L"MIDI ERROR";
 #ifdef _WINDOWS
-    midiOutGetErrorText(MidiError, midiErrorBuffer, ARRAYSIZE(midiErrorBuffer)-1);
+	midiOutGetErrorText(MidiError, midiErrorBuffer, ARRAYSIZE(midiErrorBuffer) - 1);
 #endif
-    // report the error
-    err_logo(MIDI_GENERAL, make_strnode(midiErrorBuffer));
+	// report the error
+	err_logo(MIDI_GENERAL, make_strnode(midiErrorBuffer));
 }
 
 
 NODE *lmidiopen(NODE *args)
 {
 #ifdef _WINDOWS
-    if (hMidiOut != NULL)
-    {
-        // The device is already open.
-        err_logo(MIDI_DEVICE_ALREADY_OPEN, NIL);
-        return Unbound;
-    }
+	if (hMidiOut != NULL)
+	{
+		// The device is already open.
+		err_logo(MIDI_DEVICE_ALREADY_OPEN, NIL);
+		return Unbound;
+	}
 
-    // The device is not already open, so open it.
-    UINT id = MIDIMAPPER;
+	// The device is not already open, so open it.
+	UINT id = MIDIMAPPER;
 
-    if (args != NIL)
-    {
-        id = int_arg(args);
-        if (stopping_flag == THROWING)
-        {
-            // non-integer input
-            return Unbound;
-        }
-        if (id > midiOutGetNumDevs())
-        {
-            err_logo(MIDI_INVALID_DEVICE, NIL);
-            return Unbound;
-        }
-    }
+	if (args != NIL)
+	{
+		id = int_arg(args);
+		if (stopping_flag == THROWING)
+		{
+			// non-integer input
+			return Unbound;
+		}
+		if (id > midiOutGetNumDevs())
+		{
+			err_logo(MIDI_INVALID_DEVICE, NIL);
+			return Unbound;
+		}
+	}
 
 	MIDIOUTCAPS moc = { 0 };
-    UINT MidiError = midiOutGetDevCaps(id, &moc, sizeof(moc));
-    if (!MidiError) 
-    {
-        MidiError = midiOutOpen(&hMidiOut, id, NULL, 0L, 0L);
-    }
+	UINT MidiError = midiOutGetDevCaps(id, &moc, sizeof(moc));
+	if (!MidiError)
+	{
+		MidiError = midiOutOpen(&hMidiOut, id, NULL, 0L, 0L);
+	}
 
-    if (MidiError)
-    {
-        // report the midi error
-        ThrowGeneralMidiError(MidiError);
-        return Unbound;
-    }
+	if (MidiError)
+	{
+		// report the midi error
+		ThrowGeneralMidiError(MidiError);
+		return Unbound;
+	}
 
-    NODE * targ = make_strnode(moc.szPname);
-    NODE * val = parser(targ, false);
-    return val;
+	NODE * targ = make_strnode(moc.szPname);
+	NODE * val = parser(targ, false);
+	return val;
 #else
-    return Unbound;
+	return Unbound;
 #endif
-}
+	}
 
 NODE *lmidiclose(NODE *  /*args*/)
 {
 #ifdef _WINDOWS
-    if (hMidiOut == NULL)
-    {
-        // the MIDI device isn't open
-        err_logo(MIDI_NOT_OPEN, NIL);
-        return Unbound;
-    }
+	if (hMidiOut == NULL)
+	{
+		// the MIDI device isn't open
+		err_logo(MIDI_NOT_OPEN, NIL);
+		return Unbound;
+	}
 
-    // Close the device
-    UINT MidiError = midiOutClose(hMidiOut);
-    hMidiOut = 0;
+	// Close the device
+	UINT MidiError = midiOutClose(hMidiOut);
+	hMidiOut = 0;
 
-    if (MidiError)
-    {
-        ThrowGeneralMidiError(MidiError);
-        return Unbound;
-    }
+	if (MidiError)
+	{
+		ThrowGeneralMidiError(MidiError);
+		return Unbound;
+	}
 #endif
-    return Unbound;
+	return Unbound;
 }
 
 NODE *lmidimessage(NODE *arg)
 {
 #ifdef _WINDOWS    
-    union
-    {
-        long mylong;
-        char mybyte[4];
-    }
-    bytetolong;
+	union
+	{
+		long mylong;
+		char mybyte[4];
+	}
+	bytetolong;
 
-    if (hMidiOut == NULL)
-    {
-        // the MIDI device isn't open
-        err_logo(MIDI_NOT_OPEN, NIL);
-        return Unbound;
-    }
+	if (hMidiOut == NULL)
+	{
+		// the MIDI device isn't open
+		err_logo(MIDI_NOT_OPEN, NIL);
+		return Unbound;
+	}
 
-    NODE * args = car(arg);
+	NODE * args = car(arg);
 
-    if (!is_list(args) || (args == NIL))
-    {
-        // The input must be a list with something in it.
-        err_logo(BAD_DATA_UNREC, args);
-        return Unbound;
-    }
+	if (!is_list(args) || (args == NIL))
+	{
+		// The input must be a list with something in it.
+		err_logo(BAD_DATA_UNREC, args);
+		return Unbound;
+	}
 
-    UINT MidiError;
+	UINT MidiError;
 
-    // if not system exclusive then use shortmsg else use longmsg
-    if (int_arg(args) != 0xF0)
-    {
+	// if not system exclusive then use shortmsg else use longmsg
+	if (int_arg(args) != 0xF0)
+	{
 
-        // pack 3 bytes at a time and send them as short messages
-        arg = args;
+		// pack 3 bytes at a time and send them as short messages
+		arg = args;
 
-        while (arg != NIL)
-        {
-            bytetolong.mylong = 0L;
-            bytetolong.mybyte[0] = int_arg(arg);
-            if (cdr(arg) != NIL) bytetolong.mybyte[1] = int_arg(arg = cdr(arg));
-            if (cdr(arg) != NIL) bytetolong.mybyte[2] = int_arg(arg = cdr(arg));
-            MidiError = midiOutShortMsg(hMidiOut, bytetolong.mylong);
-            if (MidiError) break;
-            if (arg != NIL) arg = cdr(arg);
-        }
+		while (arg != NIL)
+		{
+			bytetolong.mylong = 0L;
+			bytetolong.mybyte[0] = int_arg(arg);
+			if (cdr(arg) != NIL) bytetolong.mybyte[1] = int_arg(arg = cdr(arg));
+			if (cdr(arg) != NIL) bytetolong.mybyte[2] = int_arg(arg = cdr(arg));
+			MidiError = midiOutShortMsg(hMidiOut, bytetolong.mylong);
+			if (MidiError) break;
+			if (arg != NIL) arg = cdr(arg);
+		}
 
-    }
-    else
-    {
-        // count elements in list so we can allocate buffer 
-        int i = list_length(args);
+	}
+	else
+	{
+		// count elements in list so we can allocate buffer 
+		int i = list_length(args);
 
-        /* allocate structure buffer */
-        // REVISIT: why not use malloc()?
-        HANDLE    HdrHandle = (HANDLE) GlobalAlloc(GMEM_MOVEABLE | GMEM_SHARE | GMEM_ZEROINIT, sizeof(MIDIHDR));
-        MIDIHDR * MidiOutHdr = (MIDIHDR *) GlobalLock((HGLOBAL) HdrHandle);
+		/* allocate structure buffer */
+		// REVISIT: why not use malloc()?
+		HANDLE    HdrHandle = (HANDLE)GlobalAlloc(GMEM_MOVEABLE | GMEM_SHARE | GMEM_ZEROINIT, sizeof(MIDIHDR));
+		MIDIHDR * MidiOutHdr = (MIDIHDR *)GlobalLock((HGLOBAL)HdrHandle);
 
-        HANDLE DataHandle = (HANDLE) GlobalAlloc(GMEM_MOVEABLE | GMEM_SHARE | GMEM_ZEROINIT, i);
-        LPSTR  MidiOutData = (CHAR *) GlobalLock((HGLOBAL) DataHandle);
+		HANDLE DataHandle = (HANDLE)GlobalAlloc(GMEM_MOVEABLE | GMEM_SHARE | GMEM_ZEROINIT, i);
+		LPSTR  MidiOutData = (CHAR *)GlobalLock((HGLOBAL)DataHandle);
 
-        /* pack the buffer array and set size */
-        arg = args;
+		/* pack the buffer array and set size */
+		arg = args;
 
-        for (int j = 0; j < i; j++)
-        {
-            MidiOutData[j] = int_arg(arg);
-            arg = cdr(arg);
-        }
+		for (int j = 0; j < i; j++)
+		{
+			MidiOutData[j] = int_arg(arg);
+			arg = cdr(arg);
+		}
 
-        MidiOutHdr->dwBufferLength = i;
+		MidiOutHdr->dwBufferLength = i;
 
-        MidiOutHdr->lpData = MidiOutData;
+		MidiOutHdr->lpData = MidiOutData;
 
-        /* prepare it, send it out, and unprepare it */
+		/* prepare it, send it out, and unprepare it */
 
-        MidiError = midiOutPrepareHeader(hMidiOut, MidiOutHdr, sizeof(MIDIHDR));
-        if (!MidiError) MidiError = midiOutLongMsg(hMidiOut, MidiOutHdr, sizeof(MIDIHDR));
-        if (!MidiError) MidiError = midiOutUnprepareHeader(hMidiOut, MidiOutHdr, sizeof(MIDIHDR));
+		MidiError = midiOutPrepareHeader(hMidiOut, MidiOutHdr, sizeof(MIDIHDR));
+		if (!MidiError) MidiError = midiOutLongMsg(hMidiOut, MidiOutHdr, sizeof(MIDIHDR));
+		if (!MidiError) MidiError = midiOutUnprepareHeader(hMidiOut, MidiOutHdr, sizeof(MIDIHDR));
 
-        /* free buffer and struct */
+		/* free buffer and struct */
 
-        GlobalUnlock(DataHandle);
-        GlobalFree(DataHandle);
+		GlobalUnlock(DataHandle);
+		GlobalFree(DataHandle);
 
-        GlobalUnlock(HdrHandle);
-        GlobalFree(HdrHandle);
-    }
+		GlobalUnlock(HdrHandle);
+		GlobalFree(HdrHandle);
+	}
 
-    if (MidiError)
-    {
-        // Let the user know that a midi error occured.
-        ThrowGeneralMidiError(MidiError);
-        return Unbound;
-    }
+	if (MidiError)
+	{
+		// Let the user know that a midi error occured.
+		ThrowGeneralMidiError(MidiError);
+		return Unbound;
+	}
 #endif
-    return Unbound;
+	return Unbound;
 }
 
 NODE *lmci(NODE *args)
 {
 #ifdef _WINDOWS
-    // get mci command string
-    CStringPrintedNode command(car(args));
+	// get mci command string
+	CStringPrintedNode command(car(args));
 
-    // check for optional callback routine 
+	// check for optional callback routine 
 	wxString callback;
-    if (cdr(args) != NIL)
-    {
-        callback = cnv_strnode_string(cdr(args));
-    }
+	if (cdr(args) != NIL)
+	{
+		callback = cnv_strnode_string(cdr(args));
+	}
 
 	wchar_t mciReturnBuffer[MAX_BUFFER_SIZE + 1] = { 0 };
 
-    // send out command 
-    DWORD mciError = mciSendString(
+	// send out command 
+	DWORD mciError = mciSendString(
 		(const wxString&)command,
-        mciReturnBuffer,
-        ARRAYSIZE(mciReturnBuffer)-1,
-        GetMainWindow());
+		mciReturnBuffer,
+		ARRAYSIZE(mciReturnBuffer) - 1,
+		GetMainWindow());
 
-    if (mciError != ERROR_SUCCESS)
-    {
-        // let user know about the error
+	if (mciError != ERROR_SUCCESS)
+	{
+		// let user know about the error
 		wchar_t mciErrorBuffer[MAX_BUFFER_SIZE + 1] = { 0 };
-        mciGetErrorString(mciError, mciErrorBuffer, ARRAYSIZE(mciErrorBuffer)-1);
-        ShowMessageAndStop(GetResourceString(L"LOCALIZED_ERROR_MCI"), mciErrorBuffer);
-    }
-    else
-    {
-        // if something was returned then return it to user 
-        if (mciReturnBuffer[0] != '\0')
-        {
-            NODE * targ = make_strnode(mciReturnBuffer);
-            NODE * val = parser(targ, false);
-            return val;
-        }
-    }
+		mciGetErrorString(mciError, mciErrorBuffer, ARRAYSIZE(mciErrorBuffer) - 1);
+		ShowMessageAndStop(GetResourceString(L"LOCALIZED_ERROR_MCI"), mciErrorBuffer);
+	}
+	else
+	{
+		// if something was returned then return it to user 
+		if (mciReturnBuffer[0] != '\0')
+		{
+			NODE * targ = make_strnode(mciReturnBuffer);
+			NODE * val = parser(targ, false);
+			return val;
+		}
+	}
 #endif
-    return Unbound;
+	return Unbound;
 }
 
 NODE *lsettimer(NODE *args)
 {
-    // get the timer id 
-    int id = getint(ranged_integer_arg(args, 1, MAX_TIMERS - 1));
+	// get the timer id 
+	int id = getint(ranged_integer_arg(args, 1, MAX_TIMERS - 1));
 
-    // get delay
-    int delay = getint(nonnegative_int_arg(args = cdr(args)));
+	// get delay
+	int delay = getint(nonnegative_int_arg(args = cdr(args)));
 
-    // get callback
+	// get callback
 	wxString callback = cnv_strnode_string(args = cdr(args));
 
-    if (NOT_THROWING)
-    {
+	if (NOT_THROWING)
+	{
 		timer_callback[id] = callback;
 #ifdef _WINDOWS
-        // if not set sucessfully error
-        if (!::SetTimer(GetMainWindow(), id, delay, NULL))
-        {
-            err_logo(OUT_OF_MEM, NIL);
-            return Unbound;
-		}
+		// if not set sucessfully error
+		if (!::SetTimer(GetMainWindow(), id, delay, NULL))
+		{
+			err_logo(OUT_OF_MEM, NIL);
+			return Unbound;
+	}
 #else
-        //
-        timer_currents[id] = timer_intervals[id] = delay;
+		//
+		timer_currents[id] = timer_intervals[id] = delay;
 #endif
-    }
+}
 
-    return Unbound;
+	return Unbound;
 }
 
 NODE *lcleartimer(NODE *args)
 {
-    // get args
-    int id = getint(ranged_integer_arg(args, 1, MAX_TIMERS - 1));
-    if (stopping_flag == THROWING)
-    {
-        return Unbound;
-    }
-#ifdef _WINDOWS
-    // if timer was not set let user know
-    if (!::KillTimer(GetMainWindow(), id))
-    {
-        err_logo(TIMER_NOT_FOUND, NIL);
-        return Unbound;
-    }
-	else {
-		timer_callback[id].clear();
+	// get args
+	int id = getint(ranged_integer_arg(args, 1, MAX_TIMERS - 1));
+	if (stopping_flag == THROWING)
+	{
+		return Unbound;
 	}
+	timer_callback[id].clear();
+#ifdef _WINDOWS
+	// if timer was not set let user know
+	if (!::KillTimer(GetMainWindow(), id))
+	{
+		err_logo(TIMER_NOT_FOUND, NIL);
+		return Unbound;
+	}
+
 #else
-    timer_callback[id].clear();
 	timer_intervals[id] = 0;
 	timer_currents[id] = 0;
 #endif
-    return Unbound;
+	return Unbound;
 }
 
 void init_timers()
 {
-    for (size_t i = 0; i < MAX_TIMERS; i++)
-    {
-        timer_intervals[i] = 0;
-        timer_currents[i] = 0;
-        timer_callback[i].clear();
-    }
+	for (size_t i = 0; i < MAX_TIMERS; i++)
+	{
+		timer_intervals[i] = 0;
+		timer_currents[i] = 0;
+		timer_callback[i].clear();
+	}
 #ifndef _WINDOWS
-    signal(SIGALRM, SIGALRM_Handler);
- 
-    struct itimerval new_value, old_value;
-    new_value.it_value.tv_sec = 0;
-    new_value.it_value.tv_usec = 1;
-    new_value.it_interval.tv_sec = 0;
-    new_value.it_interval.tv_usec = 1000;
-    setitimer(ITIMER_REAL, &new_value, &old_value);
+	signal(SIGALRM, SIGALRM_Handler);
+
+	struct itimerval new_value, old_value;
+	new_value.it_value.tv_sec = 0;
+	new_value.it_value.tv_usec = 1;
+	new_value.it_interval.tv_sec = 0;
+	new_value.it_interval.tv_usec = 1000;
+	setitimer(ITIMER_REAL, &new_value, &old_value);
 #endif
 }
 
@@ -460,11 +458,11 @@ void halt_all_timers()
 #ifdef _WINDOWS
 		::KillTimer(GetMainWindow(), id);
 #else
-        timer_intervals[id] = 0;
-        timer_currents[id] = 0;
+		timer_intervals[id] = 0;
+		timer_currents[id] = 0;
 #endif
 		//timer_callback[id].clear();
-	}
+}
 }
 void clear_all_timers()
 {
@@ -479,7 +477,7 @@ void clear_all_timers()
 		timer_currents[id] = 0;
 #endif
 		timer_callback[id].clear();
-	}
+}
 }
 void uninitialize_timers()
 {
@@ -488,19 +486,18 @@ void uninitialize_timers()
 
 NODE *lplaywave(NODE *args)
 {
+	CStringPrintedNode fileName(car(args));
 
-    CStringPrintedNode fileName(car(args));
+	int flag = getint(nonnegative_int_arg(args = cdr(args)));
 
-    int flag = getint(nonnegative_int_arg(args = cdr(args)));
+	if (((const wxString&)fileName).length() == 0)
+	{
+		sndPlaySound(NULL, flag);
+	}
+	else
+	{
+		sndPlaySound((const wxString&)fileName, flag);
+	}
 
-    if (((const wxString&)fileName).length() == 0)
-    {
-        sndPlaySound(NULL, flag);
-    }
-    else
-    {
-        sndPlaySound((const wxString&)fileName, flag);
-    }
-
-    return Unbound;
+	return Unbound;
 }
--- startup.cpp
+++ startup.cpp
@@ -20,30 +20,31 @@
 
 #include "pch.h"
 #ifndef USE_PRECOMPILED_HEADER
-    #ifndef WX_PURE
-        #include <windows.h>
+#ifndef WX_PURE
+#include <windows.h>
 
-        #ifdef max // MS compilers #define max in windows.h
-            #undef max
-        #endif
-    #endif
+#ifdef max 
+ // MS compilers #define max in windows.h
+#undef max
+#endif
+#endif
 
-    #include <algorithm>
-    #include <string.h>
+#include <algorithm>
+#include <string.h>
 
-    #include <wx/defs.h>
-    #include <wx/string.h>
-    #include <wx/filename.h>
+#include <wx/defs.h>
+#include <wx/string.h>
+#include <wx/filename.h>
 
-    #include "startup.h"
-    #include "activearea.h"
-    #include "graphwin.h"
-    #include "utils.h"
-    #include "FMSLogo.h"
-    #include "stringadapter.h"
-//    #include "debugheap.h"
+#include "startup.h"
+#include "activearea.h"
+#include "graphwin.h"
+#include "utils.h"
+#include "FMSLogo.h"
+#include "stringadapter.h"
+ //    #include "debugheap.h"
 
-    #include "localizedstrings.h"
+#include "localizedstrings.h"
 #endif
 
 Color dfld;                        // Current flood color
@@ -63,7 +64,7 @@ wxString g_FmslogoBaseDirectory; // The directory that contains fmslogo.exe
 
 // Creates a unique filename relative to TempPath
 
-wxString GetFullText(FullTextCallBack* ftcb, int maxBufferSize) 
+wxString GetFullText(FullTextCallBack* ftcb, int maxBufferSize)
 {
 	wxString result;
 	if (ftcb != 0) {
@@ -71,9 +72,9 @@ wxString GetFullText(FullTextCallBack* ftcb, int maxBufferSize)
 
 		int ret = 0;
 		wchar_t* buffer = 0;
-		
+
 		do {
-			buffer = (wchar_t*)realloc(buffer, (length + 1)*sizeof(wchar_t));
+			buffer = (wchar_t*)realloc(buffer, (length + 1) * sizeof(wchar_t));
 			if (buffer != 0) {
 				buffer[length] = L'\0';
 
@@ -92,7 +93,7 @@ wxString GetFullText(FullTextCallBack* ftcb, int maxBufferSize)
 			}
 		} while (true);
 
-		if (ret > 0 &&buffer!=0) {
+		if (ret > 0 && buffer != 0) {
 			result = buffer;
 		}
 		if (buffer != 0) {
@@ -113,7 +114,7 @@ wxString GetFullTextReversed(FullTextCallBackReversed* ftcb, int maxBufferSize)
 
 		do {
 			buffer = new wchar_t[length];
-			ret = ftcb(buffer,length);
+			ret = ftcb(buffer, length);
 			if (ret < length) {
 				break;
 			}
@@ -133,119 +134,119 @@ wxString GetFullTextReversed(FullTextCallBackReversed* ftcb, int maxBufferSize)
 }
 void init_graphics()
 {
-    // set appropriate default colors
-    pcolor = 0x00000000;
-    scolor = 0x00FFFFFF;
-    fcolor = 0x00000000;
+	// set appropriate default colors
+	pcolor = 0x00000000;
+	scolor = 0x00FFFFFF;
+	fcolor = 0x00000000;
 
-    dfld.red   = 0x00;
-    dfld.green = 0x00;
-    dfld.blue  = 0x00;
+	dfld.red = 0x00;
+	dfld.green = 0x00;
+	dfld.blue = 0x00;
 
-    dscn.red   = 0xFF;
-    dscn.green = 0xFF;
-    dscn.blue  = 0xFF;
+	dscn.red = 0xFF;
+	dscn.green = 0xFF;
+	dscn.blue = 0xFF;
 
 #ifndef WX_PURE
-    // init the font structure
-    FontRec.lfHeight         = 24;
-    FontRec.lfWidth          = 0;
-    FontRec.lfOrientation    = 0;
-    FontRec.lfWeight         = 400;
-    FontRec.lfItalic         = 0;
-    FontRec.lfUnderline      = 0;
-    FontRec.lfStrikeOut      = 0;
-    FontRec.lfCharSet        = ANSI_CHARSET;
-    FontRec.lfOutPrecision   = OUT_DEFAULT_PRECIS;
-    FontRec.lfClipPrecision  = CLIP_DEFAULT_PRECIS;
-    FontRec.lfQuality        = DEFAULT_QUALITY;
-    FontRec.lfPitchAndFamily = DEFAULT_PITCH;
+	// init the font structure
+	FontRec.lfHeight = 24;
+	FontRec.lfWidth = 0;
+	FontRec.lfOrientation = 0;
+	FontRec.lfWeight = 400;
+	FontRec.lfItalic = 0;
+	FontRec.lfUnderline = 0;
+	FontRec.lfStrikeOut = 0;
+	FontRec.lfCharSet = ANSI_CHARSET;
+	FontRec.lfOutPrecision = OUT_DEFAULT_PRECIS;
+	FontRec.lfClipPrecision = CLIP_DEFAULT_PRECIS;
+	FontRec.lfQuality = DEFAULT_QUALITY;
+	FontRec.lfPitchAndFamily = DEFAULT_PITCH;
 
-	wcsncpy(FontRec.lfFaceName, L"Arial",ARRAYSIZE(FontRec.lfFaceName)-1);
+	wcsncpy(FontRec.lfFaceName, L"Arial", ARRAYSIZE(FontRec.lfFaceName) - 1);
 #endif
 
-    // Set handy rectangle of full bitmap
-    g_FullRect.SetWidth(BitMapWidth);
-    g_FullRect.SetHeight(BitMapHeight);
+	// Set handy rectangle of full bitmap
+	g_FullRect.SetWidth(BitMapWidth);
+	g_FullRect.SetHeight(BitMapHeight);
 
-    // turtle coords are in center
-    xoffset = BitMapWidth  / 2;
-    yoffset = BitMapHeight / 2;
+	// turtle coords are in center
+	xoffset = BitMapWidth / 2;
+	yoffset = BitMapHeight / 2;
 
-    // Init active area even if off
-    g_PrinterAreaXLow  = -BitMapWidth  / 2;
-    g_PrinterAreaXHigh = +BitMapWidth  / 2;
-    g_PrinterAreaYLow  = -BitMapHeight / 2;
-    g_PrinterAreaYHigh = +BitMapHeight / 2;
+	// Init active area even if off
+	g_PrinterAreaXLow = -BitMapWidth / 2;
+	g_PrinterAreaXHigh = +BitMapWidth / 2;
+	g_PrinterAreaYLow = -BitMapHeight / 2;
+	g_PrinterAreaYHigh = +BitMapHeight / 2;
 
-    g_PrinterAreaPixels = std::max(BitMapWidth, BitMapHeight) / 8;
+	g_PrinterAreaPixels = std::max(BitMapWidth, BitMapHeight) / 8;
 
-    // init paths to library and help files based on location of .EXE
-    wxString fmslogoDirectory = g_FmslogoBaseDirectory;
-    wxString fmslogoResourceDirectory = fmslogoDirectory;
+	// init paths to library and help files based on location of .EXE
+	wxString fmslogoDirectory = g_FmslogoBaseDirectory;
+	wxString fmslogoResourceDirectory = fmslogoDirectory;
 #ifdef __APPLE__
-    fmslogoResourceDirectory += wxString(L"../Resources/");
+	fmslogoResourceDirectory += wxString(L"../Resources/");
 #endif
 
-    const wxFileName libPathName(fmslogoResourceDirectory + wxString(L"logolib/"));
-    g_LibPathName  = libPathName.GetPathWithSep();
+	const wxFileName libPathName(fmslogoResourceDirectory + wxString(L"logolib/"));
+	g_LibPathName = libPathName.GetPathWithSep();
 
-    const wxFileName helpFileName(fmslogoResourceDirectory + wxString(L"logohelp.chm"));
-    g_HelpFileName = helpFileName.GetFullPath();
-	
+	const wxFileName helpFileName(fmslogoResourceDirectory + wxString(L"logohelp.chm"));
+	g_HelpFileName = helpFileName.GetFullPath();
+
 	wxString tp;
 
 #ifdef WX_PURE
-    const char * tempPath = getenv("TMP");
-    if (tempPath == NULL)
-    {
-        tempPath = "~";
-    }
+	const char * tempPath = getenv("TMP");
+	if (tempPath == NULL)
+	{
+		tempPath = "~";
+	}
 	tp = tempPath;
 #else
 	tp = GetFullText(GetTempPath);
 
 
 
-    bool  tempPathIsValid = false;
+	bool  tempPathIsValid = false;
 
-    if (tp.Length()>0)
-    {
+	if (tp.Length() > 0)
+	{
 		if (!tp.EndsWith(L"\\")) {
 			tp += L"\\";
 		}
-        DWORD tempPathAttributes = GetFileAttributes(tp);
-        if (tempPathAttributes != 0xFFFFFFFF)
-        {
-            // tempPath must be a directory that we can write to
-            if ( (tempPathAttributes & FILE_ATTRIBUTE_DIRECTORY) &&
-                 !(tempPathAttributes & FILE_ATTRIBUTE_READONLY))
-            {
-                tempPathIsValid = true;
-            }
-        }
-    }
+		DWORD tempPathAttributes = GetFileAttributes(tp);
+		if (tempPathAttributes != 0xFFFFFFFF)
+		{
+			// tempPath must be a directory that we can write to
+			if ((tempPathAttributes & FILE_ATTRIBUTE_DIRECTORY) &&
+				!(tempPathAttributes & FILE_ATTRIBUTE_READONLY))
+			{
+				tempPathIsValid = true;
+			}
+		}
+	}
 
-    if (!tempPathIsValid)
-    {
-        // warn the user that no TMP variable was defined.
-        MessageBox(
-            0,
+	if (!tempPathIsValid)
+	{
+		// warn the user that no TMP variable was defined.
+		MessageBox(
+			0,
 			GetResourceString(L"LOCALIZED_ERROR_TMPNOTDEFINED"),
 			GetResourceString(L"LOCALIZED_WARNING"),
-            MB_OK);
+			MB_OK);
 		tp = L"C:\\";
-    }
+	}
 #endif
 	TempBmpName = tp + L"fmslogo.bmp";
 	TempPathName = tp + L"fmslogo.lgo";
 	TempClipName = tp + L"fmslogo.clp";
 
-    g_PrinterAreaXLow   = GetConfigurationInt(L"Printer.Xlow",  -BitMapWidth  / 2);
-    g_PrinterAreaXHigh  = GetConfigurationInt(L"Printer.XHigh", +BitMapWidth  / 2);
-    g_PrinterAreaYLow   = GetConfigurationInt(L"Printer.Ylow",  -BitMapHeight / 2);
-    g_PrinterAreaYHigh  = GetConfigurationInt(L"Printer.YHigh", +BitMapHeight / 2);
-    g_PrinterAreaPixels = GetConfigurationInt(L"Printer.Pixels", std::max(BitMapWidth, BitMapHeight) / 8);
+	g_PrinterAreaXLow = GetConfigurationInt(L"Printer.Xlow", -BitMapWidth / 2);
+	g_PrinterAreaXHigh = GetConfigurationInt(L"Printer.XHigh", +BitMapWidth / 2);
+	g_PrinterAreaYLow = GetConfigurationInt(L"Printer.Ylow", -BitMapHeight / 2);
+	g_PrinterAreaYHigh = GetConfigurationInt(L"Printer.YHigh", +BitMapHeight / 2);
+	g_PrinterAreaPixels = GetConfigurationInt(L"Printer.Pixels", std::max(BitMapWidth, BitMapHeight) / 8);
 }
 
 void uninit_graphics()
